84f3cad989753f80a3e94a0ec6ec67d8a5bef494
Daiki Mizukami <tesaguriguma@gmail.com>
2023-01-29 14:44:06 +0900

    stream-to-list.rb: Use Sample Stream v2
    Turned out that Stripeaming API v1 has already been sunset.

--- stream-to-list.rb
+++ stream-to-list.rb
@@ -7,6 +7,7 @@
 
 list = Integer(ARGV[0])
 
+require 'net/http'
 require 'set'
 
 require 'twitter'
@@ -14,9 +15,11 @@ require 'twurl'
 
 MAX_LIST_LENGTH = 5000
 
+twurlrc = Twurl::RCFile.new
+(username, consumer_key) = twurlrc.default_profile
+bearer = twurlrc.bearer_tokens.fetch(consumer_key)
+
 rest = Twitter::REST::Client.new do |client|
-  twurlrc = Twurl::RCFile.new
-  (username, consumer_key) = twurlrc.default_profile
   profile = twurlrc.profiles.fetch(username).fetch(consumer_key)
   client.consumer_key = profile.fetch('consumer_key')
   client.consumer_secret = profile.fetch('consumer_secret')
@@ -37,47 +40,106 @@ finish if users.length >= MAX_LIST_LENGTH
 
 stack = []
 current_user = nil
+yielder = nil
 
-adder = Thread.new do
-  until users.length >= MAX_LIST_LENGTH
+uri = URI('https://api.twitter.com/2/tweets/sample/stream?tweet.fields=author_id')
+request = Net::HTTP::Get.new(uri)
+request['Authorization'] = "Bearer #{bearer}"
+define_singleton_method('connect_stream') do
+  return if yielder&.alive? and not yielder.status == 'aborting'
+
+  yielder = Thread.new do
+    Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+      http.request(request) do |response|
+        response.value
+        line = String.new
+        response.read_body do |chunk|
+          while i = chunk.index("\n")
+            line.concat(chunk.slice!(..i))
+            line.strip!
+            next if line.empty?
+
+            begin
+              begin
+                json = JSON.parse(line)
+              rescue => e
+                STDERR.puts <<EOS
+Unable to parse stream message: #{e.class.name} (#{e.message})
+input=#{line}
+EOS
+                next
+              end
+
+              unless user = Integer(json&.[]('data')&.[]('author_id'), 10, exception: false)
+                STDERR.puts "Unknown stream message: #{line}"
+                next
+              end
+
+              unless [users, stack, [current_user]].any? {|enum| enum.include? user }
+                stack.push user
+                if stack.length > MAX_LIST_LENGTH - users.length
+                  # Prevent the stack from having excessive # of elements while sleeping
+                  stack.shift
+                end
+                Thread.main.run
+              end
+            ensure
+              line = String.new
+            end
+          end
+          line.concat(chunk)
+        end
+      end
+    end
+  end
+  yielder.abort_on_exception = true
+
+  nil
+end
+
+KEEP_ALIVE = 5 * 60
+
+rejection_count = 0
+until users.length >= MAX_LIST_LENGTH
+  unless current_user = stack.pop
+    connect_stream
     sleep until current_user = stack.pop
+  end
 
-    begin
-      rest.add_list_member(list, current_user)
-      puts "+#{current_user}"
-      users.add current_user
-    rescue Twitter::Error::TooManyRequests => e
-      wait = e.rate_limit.reset_in
+  begin
+    rest.add_list_member(list, current_user)
+    puts "+#{current_user}"
+    users.add current_user
+    rejection_count = 0
+  rescue Twitter::Error::TooManyRequests => e
+    wait = e.rate_limit.reset_in
+    yielder.kill if wait > KEEP_ALIVE
+    STDERR.puts "Will retry in #{wait} seconds..."
+    loop do
+      sleep wait
+      break if (wait = e.rate_limit.reset_in) <= 0
+    end
+    # Dropping the current user as we're going to have a bunch of new users anyway
+    next
+  rescue Twitter::Error => e
+    STDERR.puts "Unable to add user `#{current_user}` to the List: #{e.class.name} (#{e.message})"
+    if e.is_a? Twitter::Error::Forbidden
+      # Twitter seems to reject bulk additions to a List with 403
+      wait = 1 << rejection_count
+      yielder.kill if wait > KEEP_ALIVE
+      retry_at = Time.now + wait
       STDERR.puts "Will retry in #{wait} seconds..."
       loop do
-        sleep wait
-        break if (wait = e.rate_limit.reset_in) <= 0
+        sleep wait.ceil
+        break if (wait = retry_at - Time.now) <= 0
       end
-      # Dropping the current user as we're going to have a bunch of new users anyway
-      next
-    rescue Twitter::Error => e
-      STDERR.puts "Unable to add user `#{current_user}` to the List: #{e.class.name} (#{e.message})"
+      rejection_count += 1
     end
+    next
   end
 
-  finish
+  # Reconnect to the stream on successful response: we'll likely need fresh users then
+  connect_stream
 end
-adder.abort_on_exception = true
 
-stream = Twitter::Streaming::Client.new do |client|
-  client.consumer_key = rest.consumer_key
-  client.consumer_secret = rest.consumer_secret
-  client.access_token = rest.access_token
-  client.access_token_secret = rest.access_token_secret
-end
-stream.sample do |t|
-  next unless t.is_a? Twitter::Tweet
-  unless [users, stack, [current_user]].any? {|enum| enum.include? t.user.id }
-    stack.push t.user.id
-    if stack.length > MAX_LIST_LENGTH - users.length
-      # Prevent the stack from having excessive # of elements while sleeping
-      stack.shift
-    end
-    adder.run
-  end
-end
+finish
